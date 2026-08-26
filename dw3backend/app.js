225229fd69e8e0fe0000dabfafda9fd26a207ccd
Paulo Junior <paulojunior6886@gmail.com>
2026-08-26 07:17:39 -0300

    Implementação Back-End

--- dw3backend/app.js
+++ dw3backend/app.js
@@ -1,9 +1,25 @@
+//-- arquivo: app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
-const app = express(); const port = 40000;
-
+const router = require('./routes/router');
+const app = express();
+const port = 40000;
+// Configuração do CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+    next();
+});
+app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(express.json());
+//@ Utiliza o routerApp configurado em ./routes/route.js
+app.use(router);
 app.listen(port, () => {
-    console.log(`App listening at port ${port}`);
+    console.log(`App listening at port ${port}`)
 })
+//-- $
